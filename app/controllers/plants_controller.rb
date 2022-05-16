d9daa0b15bc99fe1c77d4ca8f15e1f5cda2a2338
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-05-16 12:22:53 -0400

    commit tree c7008b13f0c17cc95d5c08e56815dad2cb0120a9

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,19 +8,49 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
+  rescue ActiveRecord::RecordNotFound
+    render_not_found_response
   end
 
   # POST /plants
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  rescue ActiveRecord::RecordNotFound
+    render_not_found_response
+  end
+
+  #PATCH /plants
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  rescue ActiveRecord::RecordNotFound
+    render_not_found_response
+  end
+
+  #DELETE /plants
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  rescue ActiveRecord::RecordNotFound
+    render_not_found_response
   end
 
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
   end
 end
